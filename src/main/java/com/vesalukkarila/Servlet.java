2a097a304eae4cbd97f75026728e857af2b4725e
Vesa Lukkarila <github@vesalukkarila.com>
2024-05-31 18:12:37 +0300

    add endpoint to servlet

--- src/main/java/com/vesalukkarila/Servlet.java
+++ src/main/java/com/vesalukkarila/Servlet.java
@@ -1,6 +1,5 @@
 package com.vesalukkarila;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,17 +10,22 @@ public class Servlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("text/html; charset=UTF-8");
-        resp.getWriter().print(
-                """
-                        <html>
-                        <body>
-                        <h1> Hello from hand made servlet</h1>
-                        <p>This is the first one</p>
-                        </body>
-                        </html>
-                        
-                        """
-        );
+        if(req.getRequestURI().equals("/")) {
+            resp.setContentType("text/html; charset=UTF-8");
+            resp.getWriter().print(
+                    """
+                            <html>
+                            <body>
+                            <h1> Hello from hand made servlet</h1>
+                            <p>This is the first one</p>
+                            </body>
+                            </html>
+                                                    
+                            """);
+        }
+        else if (req.getRequestURI().equalsIgnoreCase("/invoices")) {
+            resp.setContentType("application/json; charset=UTF-8");
+            resp.getWriter().print("[]");
+        }
     }
 }
