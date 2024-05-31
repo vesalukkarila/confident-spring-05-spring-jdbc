f14c5ebe7e290cda0306a18ed62d5ec45601854f
Vesa Lukkarila <github@vesalukkarila.com>
2024-05-31 16:08:01 +0300

    add and startup tomcat

--- src/main/java/com/vesalukkarila/ApplicationLauncher.java
+++ src/main/java/com/vesalukkarila/ApplicationLauncher.java
@@ -1,8 +1,24 @@
 package com.vesalukkarila;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
 public class ApplicationLauncher {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws LifecycleException {
+        //server&servlet container
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context ctx = tomcat.addContext("", null);
+        //add servlet to container
+        Wrapper servlet = Tomcat.addServlet(ctx, "servlet", new Servlet());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*");
+
+        tomcat.start();
     }
 }
