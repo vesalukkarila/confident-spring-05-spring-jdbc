0e2fb12fd3ddb5f3a1274ec75660f43647438785
Vesa Lukkarila <github@vesalukkarila.com>
2024-06-03 15:44:09 +0300

    add Configuration file, make ObjectMapper a Bean, refactor related code

--- src/main/java/com/vesalukkarila/web/Servlet.java
+++ src/main/java/com/vesalukkarila/web/Servlet.java
@@ -1,14 +1,29 @@
 package com.vesalukkarila.web;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vesalukkarila.context.Application;
+import com.vesalukkarila.context.ApplicationConfiguration;
 import com.vesalukkarila.model.Invoice;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 
 public class Servlet extends HttpServlet {
+
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+        this.objectMapper = context.getBean(ObjectMapper.class);
+
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -27,7 +42,7 @@ public class Servlet extends HttpServlet {
         }
         else if (req.getRequestURI().equalsIgnoreCase("/invoices")){
             resp.setContentType("application/json; charset=UTF-8");
-            String json = Application.objectMapper.writeValueAsString(Application.invoiceService.getInvoices());
+            String json = objectMapper.writeValueAsString(Application.invoiceService.getInvoices());
             resp.getWriter().print(json);
         }
     }
@@ -41,7 +56,7 @@ public class Servlet extends HttpServlet {
             Invoice invoice = Application.invoiceService.create(userId, amount);
 
             resp.setContentType("application/json; charset=UTF-8");
-            String json = Application.objectMapper.writeValueAsString(invoice);
+            String json = objectMapper.writeValueAsString(invoice);
             resp.getWriter().print(json);
         }else{
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
