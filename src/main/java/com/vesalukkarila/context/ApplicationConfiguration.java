f2c8be116d250c4bd30170fe2cf6b6d52024c68e
Vesa Lukkarila <github@vesalukkarila.com>
2024-06-03 17:56:53 +0300

    add properties file that runs in dev enviroment

--- src/main/java/com/vesalukkarila/context/ApplicationConfiguration.java
+++ src/main/java/com/vesalukkarila/context/ApplicationConfiguration.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.*;
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)//by default scans from this package
 @PropertySource("classpath:/application.properties")
+@PropertySource(value = "classpath:/application-${spring.profiles.active{}.properties",
+                        ignoreResourceNotFound = true)
+//above: bottom PropertySource has precedence over the one at the top
 public class ApplicationConfiguration {
 
     //singleton already by default, for demonstration purposes
