bddf65eaa30fede590cc1b02868f52d9cd87d22c
Vesa Lukkarila <github@vesalukkarila.com>
2024-06-04 10:33:19 +0300

    add endpoint, @EnableWebMvc

--- src/main/java/com/vesalukkarila/context/ApplicationConfiguration.java
+++ src/main/java/com/vesalukkarila/context/ApplicationConfiguration.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vesalukkarila.ApplicationLauncher;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)//by default scans from this package
 @PropertySource("classpath:/application.properties")
 @PropertySource(value = "classpath:/application-${spring.profiles.active{}.properties",
                         ignoreResourceNotFound = true)
-//above: bottom PropertySource has precedence over the one at the top
+@EnableWebMvc //initializes spring webmvc with default configurations, enables use of dependencies in classpath
 public class ApplicationConfiguration {
 
     //singleton already by default, for demonstration purposes
--- src/main/java/com/vesalukkarila/web/InvoiceController.java
+++ src/main/java/com/vesalukkarila/web/InvoiceController.java
@@ -1,15 +1,32 @@
 package com.vesalukkarila.web;
 
+import com.vesalukkarila.model.Invoice;
+import com.vesalukkarila.service.InvoiceService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class InvoiceController {
 
+    private final InvoiceService invoiceService;
+
+    public InvoiceController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
     @GetMapping("/")
-    @ResponseBody
     public String index() {
         return "Hello from webmvc controller";
+    }
+
+    /*@EnableWebMvc in config enables Jackson to work behind the scenes
+    * and converting Java object <-> JSON*/
+    @GetMapping("/invoices")
+    public List<Invoice> getInvoices() {
+        return invoiceService.getInvoices();
     }
 }
