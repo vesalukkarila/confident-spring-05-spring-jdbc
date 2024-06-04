892bbfa530624b8c5dbbbbcba882fe0d3787b2f9
Vesa Lukkarila <github@vesalukkarila.com>
2024-06-04 10:05:29 +0300

    create dispatcherservlet, hook it up with tomcat

--- src/main/java/com/vesalukkarila/ApplicationLauncher.java
+++ src/main/java/com/vesalukkarila/ApplicationLauncher.java
@@ -1,10 +1,15 @@
 package com.vesalukkarila;
 
+import com.vesalukkarila.context.ApplicationConfiguration;
 import com.vesalukkarila.web.Servlet;
+import jakarta.servlet.ServletContext;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class ApplicationLauncher {
 
@@ -14,12 +19,24 @@ public class ApplicationLauncher {
         tomcat.setPort(8080);
         tomcat.getConnector();
 
-        Context ctx = tomcat.addContext("", null);
+        Context tomcatCtx = tomcat.addContext("", null);
+
+        WebApplicationContext appCtx = createApplicationContext(tomcatCtx.getServletContext());
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(appCtx);
         //add servlet to container
-        Wrapper servlet = Tomcat.addServlet(ctx, "servlet", new Servlet());
+        Wrapper servlet = Tomcat.addServlet(tomcatCtx, "dispatcherServlet", dispatcherServlet);
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/*");
 
         tomcat.start();
+    }
+
+    private static WebApplicationContext createApplicationContext(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+        webCtx.register(ApplicationConfiguration.class);
+        webCtx.setServletContext(servletContext);
+        webCtx.refresh();
+        webCtx.registerShutdownHook();
+        return webCtx;
     }
 }
