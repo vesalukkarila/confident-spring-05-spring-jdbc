b7ce7ca8f7b50d075966b2df6e81ad45a536b454
Vesa Lukkarila <github@vesalukkarila.com>
2024-06-05 14:30:54 +0300

    add post endpoint for form with fake login

--- src/main/java/com/vesalukkarila/web/WebController.java
+++ src/main/java/com/vesalukkarila/web/WebController.java
@@ -4,6 +4,8 @@ import com.vesalukkarila.web.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
@@ -22,8 +24,18 @@ public class WebController {
 
     //this serves the login.html and allready provides backing bean for incoming data from form
     @GetMapping("/login")
-    public  String login(Model model) {
+    public String login(Model model) {
         model.addAttribute("loginForm", new LoginForm());
         return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm loginForm, Model model) {
+        //faking login
+        if (loginForm.getUsername().equalsIgnoreCase(loginForm.getPassword())) {
+            return "redirect:/";
+        }
+        model.addAttribute("invalidCredentials", true);
+        return "login.html"; //spring will issue http 302 redirect to the path you are returning
     }
 }
