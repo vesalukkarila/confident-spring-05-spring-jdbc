30eb4752edc6cf3dcfb842ce1d3be5cf57f3c3c0
Vesa Lukkarila <github@vesalukkarila.com>
2024-06-05 15:28:37 +0300

    add validation for username and password, refactor login.html accordingly

--- src/main/java/com/vesalukkarila/web/WebController.java
+++ src/main/java/com/vesalukkarila/web/WebController.java
@@ -1,8 +1,10 @@
 package com.vesalukkarila.web;
 
 import com.vesalukkarila.web.forms.LoginForm;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,13 +31,17 @@ public class WebController {
         return "login.html";
     }
 
+    /*without use of bindingresult exception would be thrown*/
     @PostMapping("/login")
-    public String login(@ModelAttribute LoginForm loginForm, Model model) {
+    public String login(@ModelAttribute @Valid LoginForm loginForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "login.html";
+        }
         //faking login
         if (loginForm.getUsername().equalsIgnoreCase(loginForm.getPassword())) {
-            return "redirect:/";
+            return "redirect:/";//redirects to '/',spring will issue http 302 redirect to the path you are returning
         }
         model.addAttribute("invalidCredentials", true);
-        return "login.html"; //spring will issue http 302 redirect to the path you are returning
+        return "login.html";
     }
 }
